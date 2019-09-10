fdd2696bcc65c4241f57731c498c9907dbf0a047
Steven van Esch <steven_ve@live.nl>
2019-09-10 14:13:16 +0200

    Added index, noteValue function, replaced recursive with forloop function

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,33 +1,46 @@
-#import simpleaudio as sa
+# import simpleaudio as sa
+import time
 
-#wave_obj = sa.WaveObject.from_wave_file("BMR_Splash_1.wav")
-#play_obj = wave_obj.play()
+# wave_obj = sa.WaveObject.from_wave_file("BMR_Splash_1.wav")
+# play_obj = wave_obj.play()
 playSound = "dong"
 
+# play_obj.wait_done()
+
+print("<<<<<<<<<<<<<<<<<<<< BeatMachine >>>>>>>>>>>>>>>>>>>")
+
+time.sleep(1)
+
+print("""....................... Index ......................
+Notevalues:
+4 = whole note;
+2 = half note;
+1 = quarter note;
+0.5 = eight note;
+0.25 = sixteenth note;
+etc, etc, etc.
+
+Actual values are based on the BPM.
+""")
+
+# Input bpm and convert to seconds
+bpm = 60 / (float(input ("What is the bpm? ")))
+
+# Input number of repititions
 numPlaybackTimes = int(input ("How many times do you want to play the sample? "))
-list = []
 
-#play_obj.wait_done()
+# Enter list with note values
+note_list = []
 
-# Add noteLength to each note
 for i in range(numPlaybackTimes):
-    noteLength = float(input ("Add duration in miliseconds "))
-    print(i,":", noteLength)
-    if i == (numPlaybackTimes-1):
-        print ("That was the last one! ")
-        bpm = 60000 / (int(input ("What is the bpm? ")))
-        print ("bpm in miliseconds: ", bpm)
+    note_list.append(float(input("Enter note values one by one: ")) * float(bpm))
 
-# Multiply noteLength with bpm
-
-
-
-
+# Play sound and repeat till numPlaybackTimes = 0 (forLoop)
 def playSample(numPlaybackTimes):
+    for i in range(numPlaybackTimes):
+        print (playSound)
+        time.sleep(note_list[i])
     if int(numPlaybackTimes) == 0:
         return print ("finished")
-    else:
-        print (playSound)
-        return playSample(numPlaybackTimes-1)
 
-# playSample(numPlaybackTimes)
+playSample(numPlaybackTimes)
