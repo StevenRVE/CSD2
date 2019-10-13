cff2184ce842d2ce4c937a9221110d0fde309f09
Steven van Esch <steven_ve@live.nl>
2019-10-13 21:28:11 +0200

    Added events and dictionary

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -37,59 +37,60 @@ bpm = 120
 
 bpmCorrect = False
 
-# # input function for bpm
-# def ask_BPM(bpmCorrect):
-#     while (bpmCorrect == False):
-#          try:
-#              bpm = 60 / (float(input ("What is the bpm? ")))
-#          except ValueError:
-#              print("Wrong value. Try again: ")
-#          except NameError:
-#              print("That\"s not a number. Try again: ")
-#          except TypeError:
-#              print("I need a whole number please. Try again: ")
-#          except Exception as e:
-#              print("Something went wrong. ",e)
-#              print("Try again: ")
-#          else:
-#              bpmCorrect = True
-#              print("Thanks!")
-#
-# # Check if user wants to use preset bpm or input own bpm
-# def change_BPM_yes_no(question, bpmCorrect, default="yes"):
-#     """Ask a yes/no question via input() and return their answer.
-#
-#     "question" is a string that is presented to the user.
-#     "default" is the presumed answer if the user just hits <Enter>.
-#         It must be "yes" (the default), "no" or None (meaning
-#         an answer is required of the user).
-#
-#     The "answer" return value is True for "yes" or False for "no".
-#     """
-#     valid = {"yes": True, "y": True, "ye": True,
-#              "no": False, "n": False}
-#     if default is None:
-#         prompt = " [y/n] "
-#     elif default == "yes":
-#         prompt = " [Y/n] "
-#     elif default == "no":
-#         prompt = " [y/N] "
-#     else:
-#         raise ValueError("invalid default answer: "%s"" % default)
-#
-#     while True:
-#         sys.stdout.write(question + prompt)
-#         choice = input().lower()
-#         if default is not None and choice == "":
-#             return valid[default]
-#         elif (choice in valid) == True:
-#             return valid[choice]
-#         else:
-#             sys.stdout.write("Please respond with "yes" or "no" "
-#                              "(or "y" or "n").\n")
-#
-# change_BPM_yes_no("Do you want to use the default BPM? ", bpmCorrect)
-# print(bpm)
+# input function for bpm
+def ask_BPM(bpmCorrect):
+    global bpm
+    while (bpmCorrect == False):
+         try:
+             bpm = 60 / (float(input ("What is the bpm? ")))
+         except ValueError:
+             print("Wrong value. Try again: ")
+         except NameError:
+             print("That\"s not a number. Try again: ")
+         except TypeError:
+             print("I need a whole number please. Try again: ")
+         except Exception as e:
+             print("Something went wrong. ",e)
+             print("Try again: ")
+         else:
+             bpmCorrect = True
+             print("Thanks!")
+
+# Check if user wants to use preset bpm or input own bpm
+def change_BPM_yes_no(question, bpmCorrect, default="yes"):
+    """Ask a yes/no question via input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError('invalid default answer: '%s'' % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif (choice in valid) == True:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with "yes" or "no" "
+                             "(or "y" or "n").\n")
+
+change_BPM_yes_no("Do you want to use the default BPM? ", bpmCorrect)
+print(bpm)
 
 # convert bpm to notelengths in seconds
 quarterNoteDuration = 60 / bpm
@@ -130,7 +131,7 @@ dictListBank = dictList.copy()
 loopTimes = int(input("How often do you want to play the sequence? "))
 loopedTimes = 0
 
-def playSequence(loopTimes):
+def playSequence():
     for i in range(loopTimes):
         global loopedTimes
         global dictList
@@ -147,8 +148,6 @@ def playSequence(loopTimes):
         dictionary = dictList.pop(0)
         timestamp = dictionary["timestamp"]
         sample = dictionary["instrument"]
-
-
 
         # play the sequence
         while True:
@@ -180,7 +179,7 @@ def playSequence(loopTimes):
                 # nothing to do
                 time.sleep(0.001)
 
-playSequence(loopTimes)
+playSequence()
 
 # # run playSample
 # keep_going = True
