fece8fc8e8aa2031ec65bdbec61f846a15739915
Steven van Esch <steven_ve@live.nl>
2019-10-13 19:56:26 +0200

    Added events and dictionary

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,13 +1,14 @@
-import simpleaudio as sa
-import time
-import threading as th
 import random
+import simpleaudio as sa
 import sys
+import threading as th
+import time
+
 
 # Define instrument
-Hihat = sa.WaveObject.from_wave_file("samples/16_bit/Hihat.wav")
-Kick = sa.WaveObject.from_wave_file("samples/16_bit/Kick.wav")
-Snare = sa.WaveObject.from_wave_file("samples/16_bit/Snare.wav")
+samples = [sa.WaveObject.from_wave_file("samples/16_bit/Hihat.wav"),
+            sa.WaveObject.from_wave_file("samples/16_bit/Kick.wav"),
+            sa.WaveObject.from_wave_file("samples/16_bit/Snare.wav")]
 
 # Program title
 print("_____/<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>\_____")
@@ -16,79 +17,79 @@ print("\u203e\u203e\u203e\u203e\u203e\\<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>/
 
 time.sleep(0.25)
 
-# program introduction and instruction
-print("""....................... Index ......................
-Notevalues:
-4 = whole note;
-2 = half note;
-1 = quarter note;
-0.5 = eight note;
-0.25 = sixteenth note;
-etc, etc, etc.
-
-Actual values are based on the BPM.
-
-While playing press [Enter] to finish the sequence and stop playing
-""")
+# # program introduction and instruction
+# print("""....................... Index ......................
+# Notevalues:
+# 4 = whole note;
+# 2 = half note;
+# 1 = quarter note;
+# 0.5 = eight note;
+# 0.25 = sixteenth note;
+# etc, etc, etc.
+#
+# Actual values are based on the BPM.
+#
+# While playing press [Enter] to finish the sequence and stop playing
+# """)
 
 # Input default bpm convert to seconds
 bpm = 120
 
 bpmCorrect = False
 
-# input function for bpm
-def ask_BPM(bpmCorrect):
-    while (bpmCorrect == False):
-         try:
-             bpm = 60 / (float(input ("What is the bpm? ")))
-         except ValueError:
-             print("Wrong value. Try again: ")
-         except NameError:
-             print("That\'s not a number. Try again: ")
-         except TypeError:
-             print("I need a whole number please. Try again: ")
-         except Exception as e:
-             print("Something went wrong. ",e)
-             print("Try again: ")
-         else:
-             bpmCorrect = True
-             print("Thanks!")
-
-# Check if user wants to use preset bpm or input own bpm
-def change_BPM_yes_no(question, bpmCorrect, default="yes"):
-    """Ask a yes/no question via input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif (choice in valid) == True:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
-
-change_BPM_yes_no("Do you want to use the default BPM? ", bpmCorrect)
-print(bpm)
+# # input function for bpm
+# def ask_BPM(bpmCorrect):
+#     while (bpmCorrect == False):
+#          try:
+#              bpm = 60 / (float(input ("What is the bpm? ")))
+#          except ValueError:
+#              print("Wrong value. Try again: ")
+#          except NameError:
+#              print("That\"s not a number. Try again: ")
+#          except TypeError:
+#              print("I need a whole number please. Try again: ")
+#          except Exception as e:
+#              print("Something went wrong. ",e)
+#              print("Try again: ")
+#          else:
+#              bpmCorrect = True
+#              print("Thanks!")
+#
+# # Check if user wants to use preset bpm or input own bpm
+# def change_BPM_yes_no(question, bpmCorrect, default="yes"):
+#     """Ask a yes/no question via input() and return their answer.
+#
+#     "question" is a string that is presented to the user.
+#     "default" is the presumed answer if the user just hits <Enter>.
+#         It must be "yes" (the default), "no" or None (meaning
+#         an answer is required of the user).
+#
+#     The "answer" return value is True for "yes" or False for "no".
+#     """
+#     valid = {"yes": True, "y": True, "ye": True,
+#              "no": False, "n": False}
+#     if default is None:
+#         prompt = " [y/n] "
+#     elif default == "yes":
+#         prompt = " [Y/n] "
+#     elif default == "no":
+#         prompt = " [y/N] "
+#     else:
+#         raise ValueError("invalid default answer: "%s"" % default)
+#
+#     while True:
+#         sys.stdout.write(question + prompt)
+#         choice = input().lower()
+#         if default is not None and choice == "":
+#             return valid[default]
+#         elif (choice in valid) == True:
+#             return valid[choice]
+#         else:
+#             sys.stdout.write("Please respond with "yes" or "no" "
+#                              "(or "y" or "n").\n")
+#
+# change_BPM_yes_no("Do you want to use the default BPM? ", bpmCorrect)
+# print(bpm)
 
 # convert bpm to notelengths in seconds
 quarterNoteDuration = 60 / bpm
@@ -97,37 +98,104 @@ sixteenthNoteDuration = quarterNoteDuration / 4.0
 # create a list to hold the timestamps
 timestamps = []
 
-# Import list with note values from inputList.txt
-noteList = [line.rstrip('\r\n') for line in open('inputList.txt')]
+# # Import list with note values from inputList.txt
+# noteList = [line.rstrip("\r\n") for line in open("inputList.txt")]
+#
+# # Convert strings in list to float
+# noteList = list(map(float,noteList))
+#
+# # Input number of repititions
+# numPlaybackTimes = len(noteList)
 
-# Convert strings in list to float
-noteList = list(map(float,noteList))
+# create a list with "note timestamps" specified as 16th notes, indicating
+# the time at which the sample must be played
+# timestamps16th = [0,3,4,6,9,10,13,15]
+timestamps16th = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-# Input number of repititions
-numPlaybackTimes = len(noteList)
+# transform the sixteenthTimestamps to a timestamps list with time values
+for timestamp in timestamps16th:
+  timestamps.append(timestamp * sixteenthNoteDuration)
 
-# Play sound and repeat till numPlaybackTimes = 0 (forLoop)
-def playSample(numPlaybackTimes):
-    for i in range(numPlaybackTimes):
-        play_obj = wave_obj.play()
-        time.sleep(noteList[i]*bpm)
-    if int(numPlaybackTimes) == 0:
-        print ("finished")
+dictList = []
 
-# run playSample
-keep_going = True
+# create dictionarys from timestamps and add random samples
+for stamp in timestamps:
+    stamp = {"timestamp": stamp, "instrument": random.randint(0, 2)}
+    dictList.append(stamp)
 
-def key_capture_thread():
-    global keep_going
-    input()
-    keep_going = False
+# copy dictList into new list for later re-copy
+dictListBank = dictList.copy()
 
-def run_PlaySample():
-    th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
-    while keep_going:
-        playSample(numPlaybackTimes)
+# input the amount of times that the sequence has to be looped
+loopTimes = int(input("How often do you want to play the sequence? "))
+loopedTimes = 0
 
-# run_PlaySample()
+def playSequence(loopTimes):
+    for i in range(loopTimes):
+        global loopedTimes
+        global dictList
+
+        # retrieve the startime: current time
+        startTime = time.time()
+
+        # get a random value to be used as sample index
+        randomIndex = random.randint(0, 2)
+
+        # The total duration of 1 sequence
+        totalSequenceTime = (60/bpm) * 4
+
+        dictionary = dictList.pop(0)
+        timestamp = dictionary["timestamp"]
+        sample = dictionary["instrument"]
+
+
+
+        # play the sequence
+        while True:
+            # retrieve current time
+            currentTime = time.time()
+
+            # check whether the current time is beyond the timestamp"s time,
+            # meaning it is time to play the sample
+            if((currentTime - startTime) % totalSequenceTime >= timestamp):
+                samples[sample].play()
+
+                # if there are timestamps left in the timestamps list
+                if dictList:
+                    # retrieve the next timestamp
+                    dictionary = dictList.pop(0)
+                    timestamp = dictionary["timestamp"]
+                    sample = dictionary["instrument"]
+                else:
+                    # copy the dictionary from the bank
+                    dictList = dictListBank.copy()
+                    # reset startTime
+                    startTime = time.time()
+                    print("Sequence done. ")
+                    time.sleep(sixteenthNoteDuration)
+                    break
+
+            else:
+                # short wait to prevent we"ll keep the processor busy when there"s
+                # nothing to do
+                time.sleep(0.001)
+
+playSequence(loopTimes)
+
+# # run playSample
+# keep_going = True
+#
+# def key_capture_thread():
+#     global keep_going
+#     input()
+#     keep_going = False
+#
+# def run_PlaySequence():
+#     th.Thread(target=key_capture_thread, args=(), name="key_capture_thread", daemon=True).start()
+#     while keep_going:
+#         playSequence(startTime, timestamp)
+#
+# run_PlaySequence()
 
 # Closing comment
 print("_____/<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>\_____")
