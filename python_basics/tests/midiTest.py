8e62c448b8369f30170d369536cd89da4c9582fb
Steven van Esch <steven_ve@live.nl>
2019-11-25 16:11:04 +0100

    midiTest file

--- python_basics/tests/midiTest.py
+++ python_basics/tests/midiTest.py
@@ -1,39 +1,3 @@
-# from midiutil.MidiFile import MIDIFile
-
-#
-# midiNoteList = []
-#
-# # create your MIDI object
-# mf = MIDIFile(3)     # only 1 track
-# track = 0   # the only track
-#
-# time = 0    # start at the beginning
-# mf.addTrackName(track, time, "Sample Track")
-# mf.addTempo(track, time, 120)
-#
-# # add some notes
-# channel = 0
-# volume = 100
-#
-# pitch = 60           # C4 (middle C)
-# time = 0             # start on beat 0
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-#
-# pitch = 64           # E4
-# time = 2             # start on beat 2
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-#
-# pitch = 67           # G4
-# time = 4             # start on beat 4
-# duration = 1         # 1 beat long
-# mf.addNote(track, channel, pitch, time, duration, volume)
-#
-# # write it to disk
-# with open("output.mid", 'wb') as outf:
-#     mf.writeFile(outf)
-
 from midiutil import MIDIFile
 
 degrees  = [60, 62, 64, 65, 67, 69, 71, 72] # MIDI note number
